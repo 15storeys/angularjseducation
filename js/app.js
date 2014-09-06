b3d17fdebf5ddd8cc3f250e8e585f650b1cc7956
15storeys <justin_lust@yahoo.com>
2014-09-06 20:36:25 +0100

    Add images using ng-src
    Add images using ng-src

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 	});
 	
 	var gems = [
-				{ name: 'Dodecahedron', price: 2.95, description: 'Some gems have hidden qualities', canPurchase: true, soldOut: false},
-				{ name: 'Pentagonal Gem', price: 5.95, description: 'Five five five', canPurchase: false, soldOut: false}
+				{ name: 'Dodecahedron', price: 2.95, description: 'Some gems have hidden qualities', canPurchase: true, soldOut: false, images: [{full: 'http://media-cache-ec0.pinimg.com/736x/9f/8e/cb/9f8ecb76e56906a385d0104198931759.jpg', thumbnail: 'http://media-cache-ec0.pinimg.com/736x/9f/8e/cb/9f8ecb76e56906a385d0104198931759.jpg'}]},
+				{ name: 'Pentagonal Gem', price: 5.95, description: 'Five five five', canPurchase: false, soldOut: false, images: [{full: 'http://gemsbydeni.com/store/images/190077845295.jpg', thumbnail: 'http://gemsbydeni.com/store/images/190027902819.jpg'}]}
 			];
 })();
